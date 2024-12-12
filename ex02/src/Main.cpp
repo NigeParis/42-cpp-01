69f1ecc4943e8dacf641cbc821f3427c16415456
NigeParis <nrobinso@student.42.fr>
2024-12-12 14:33:42 +0100

    ex02 finished

--- ex02/src/Main.cpp
+++ ex02/src/Main.cpp
@@ -6,15 +6,48 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:48:07 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/12/12 13:46:54 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/12/12 14:14:56 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
 
 int main(void) {
 
+	std::string str = "HI THIS IS BRAIN";
+
+	std::string *ptrStr = &str;               //pointer to str
+	std::string& refStr = str;		   //reference to str
 
 
+	std::cout << "Adresses :" << std::endl;
 
+	std::cout << "The string prints it's address: ";
+	std::cout << "'\033[32m" << &str  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+
+	std::cout << "Pointer to a string prints the address pointing to: ";
+	std::cout << "'\033[32m" << ptrStr  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+
+	std::cout << "Reference to a string prints the address referenced to: ";
+	std::cout << "'\033[32m" << &refStr  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+
+	std::cout << "Values :" << std::endl;
+
+	std::cout << "The String prints it's value: ";
+	std::cout << "'\033[31m" << str  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+
+	std::cout << "Pointer to a string prints value it's pointing to: ";
+	std::cout << "'\033[31m" << *ptrStr  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+
+	std::cout << "Reference to a string prints value it's referenced to: ";
+	std::cout << "'\033[31m" << refStr  << "\033[0m'";
+	std::cout << " of str" << std::endl;
+	
 	return (0);	
 }
---
+++ ex03/src/Main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 12:48:07 by nrobinso          #+#    #+#             */
+/*   Updated: 2024/12/12 14:26:45 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+return 0;
+}
