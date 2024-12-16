61fb64a850e5495e79a7ab54cb3ebc4039194e62
Nigel Robinson <nrobinso@paul-f4Ar8s7.clusters.42paris.fr>
2024-12-16 15:15:42 +0100

    class adjusted

--- ex05/include/Harl.hpp
+++ ex05/include/Harl.hpp
@@ -6,26 +6,36 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:04:31 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/12/16 14:46:21 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/12/16 15:12:49 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #ifndef HARL_HPP
 #define HARL_HPP
+#include <iostream>
+#include <string>
 
 class Harl {
 
     public:
 
+        typedef void (Harl::*FunPtr)();
+
         Harl(void);
         ~Harl(void);
-
+        void complain( std::string level );
 
     private:
-
+        
+        FunPtr *funptr;
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
     
 };
+
 
 
 
--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:27:14 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/12/16 14:50:15 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/12/16 15:15:20 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,33 @@ Harl::~Harl(void) {
   
 };
 
+
+void Harl::complain( std::string level ) {
+
+
+    
+};
+
+
+void Harl::error( void ) {
+
+            
+};
+
+
+void Harl::warning( void ) {
+
+            
+};
+
+
+void Harl::info( void ) {
+
+            
+};
+
+
+void Harl::debug( void ) {
+
+            
+};
