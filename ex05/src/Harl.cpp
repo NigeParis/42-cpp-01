73f5dc0661a00b4b23f37647e37318de4756f3c3
Nigel Robinson <nrobinso@paul-f4Ar8s7.clusters.42paris.fr>
2024-12-17 12:42:06 +0100

    ex05 finnished i think

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -6,14 +6,13 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 14:27:14 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/12/16 16:45:43 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/12/17 12:38:00 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-
-Harl::Harl(void)  : funptr(NULL)  {
+Harl::Harl(void) {
     
 };
 
@@ -21,39 +20,66 @@ Harl::~Harl(void) {
 
 };
 
-
-
 void Harl::error( void ) {
 
-    std::cout << "An Error ! oh" << std::endl;
+    std::cout << "\033[31m[ ERROR ]\033[0m - level" << std::endl;
+    std::cout << "This is unacceptable!"; 
+    std::cout << "I want to speak to the manager now." << std::endl;  
 
 };
-
 
 void Harl::warning( void ) {
 
-    std::cout << "Warning : !!!!!!!!" << std::endl;
+    std::cout << "\033[33m[ WARNING ]\033[0m - level" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. ";  
+    std::cout << "I’ve been coming for years ";
+    std::cout << "whereas you started working here since last month." << std::endl;  
             
 };
-
 
 void Harl::info( void ) {
 
-    std::cout << "Info ....... :-)" << std::endl;
-            
+    std::cout << "\033[35m[ INFO ]\033[0m - level" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money."; 
+    std::cout << "You didn’t put enough bacon in my burger! ";
+    std::cout << "If you did, I wouldn’t be asking for more!" << std::endl;                  
 };
-
 
 void Harl::debug( void ) {
 
-    std::cout << "Debug and Debug" << std::endl;            
+    std::cout << "\033[36m[ DEBUG ]\033[0m - level" << std::endl;
+    std::cout << "I love having extra bacon for my "; 
+    std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger.";
+    std::cout << " I really do!" << std::endl;            
 };
 
 void Harl::complain( std::string level ) {
 
-     
+    int i = 0;
+    std::string find[4] = {
 
-    
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"  
+    };
 
-    
+    void (Harl::*ptr[4] )(void) = {
+        
+        ptr[0] = &Harl::debug,
+        ptr[1] = &Harl::info,
+        ptr[2] = &Harl::warning,
+        ptr[3] = &Harl::error
+    };
+
+    while (i < 4) {
+
+        if (find[i] == level)
+            break ;
+        i++;
+    }
+    if (i > 3)
+        return ;
+    return (this->*ptr[i])();
 };
+    
--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:48:07 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/12/16 16:10:52 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/12/17 12:35:12 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@ int main( void )
 {
 	Harl test;
 
-
 	test.complain("DEBUG");
+	test.complain("ERROR");
+	test.complain("INFO");
+	test.complain("WARNING");
+	test.complain("ERRORMSG");
+	test.complain("");
+
 	return (0);
 }
